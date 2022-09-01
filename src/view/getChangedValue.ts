ac1f5bea127affafbb31c57d47f92ad37013eaea
jaceyi <yijinchun@outlook.com>
2022-09-01 16:58:07 +0800

    fix getChangedValue update order

--- src/view/getChangedValue.ts
+++ src/view/getChangedValue.ts
@@ -1,8 +1,31 @@
+// 获取指定月份有多少天
+const getDays = (year: number, month: number) => {
+    const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        days[1] = 29;
+    }
+    return days[month];
+};
+
 export default (v: { year?: number; month?: number; date?: number }, current: Date, value?: Date) => {
-    const result = new Date(value == null ? current : value);
-    const { year, month, date } = v;
+    const currentDate = new Date(value == null ? current : value);
+    const currentMonth = currentDate.getMonth();
+    const result = new Date(currentDate);
+    const { year, month = currentMonth, date } = v;
     if (year != null) result.setFullYear(year);
-    if (month != null) result.setMonth(month);
-    if (date != null) result.setDate(date);
+    if (month !== currentMonth) {
+        // 如果月没变则不更新月
+        if (getDays(currentDate.getFullYear(), currentMonth) > getDays(result.getFullYear(), month)) {
+            // 当前月的天数大于更新月的天数，先更新天再更新月
+            if (date != null) result.setDate(date);
+            result.setMonth(month);
+        } else {
+            result.setMonth(month);
+            if (date != null) result.setDate(date);
+        }
+    } else {
+        if (date != null) result.setDate(date);
+    }
+
     return result;
 };
